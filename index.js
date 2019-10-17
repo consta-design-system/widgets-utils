a7dfda16f2f4bc87a0ba7941a64dc17078195074
evless <evless56@gmail.com>
2019-10-17 14:22:12 +0300

    GINF-109: Добавить метод для получения посчитанного размера адаптивности

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+// Значение, которое никогда не меняется
+const CONSTANT_SIZE = 16
+// Значение, которое изменяется при ресайзе окна и используется для расчета всех размеров
+let baseSize
+
+const updateBaseSize = (size, element) => {
+  baseSize = size
+  element.style.setProperty('--base-size', `${size}`)
+}
+
+const getCalculatedSize = size => (size / CONSTANT_SIZE) * baseSize
+
 const calcSize = (size, isVar) => {
-  const text = isVar ? `(${size} / 16)` : `${size / 16}px`
+  const text = isVar ? `(${size} / ${CONSTANT_SIZE})` : `${size / CONSTANT_SIZE}px`
   return `calc(${text} * var(--base-size))`
 }
 
 module.exports = {
   calcSize,
+  getCalculatedSize,
+  updateBaseSize,
 }
