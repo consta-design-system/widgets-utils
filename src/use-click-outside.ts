44f8000463049356a773c011bfc28dd25caa3fd8
Alexey Fyodorov <dev.fyodorov@gmail.com>
2019-12-18 21:50:40 +0300

    GINF-555: Расширенный заголовок: Закрывать тултип кликом мимо

--- src/use-click-outside.ts
+++ src/use-click-outside.ts
@@ -1,13 +1,16 @@
 import { RefObject, useEffect } from 'react'
 
-export function useClickOutside(refs: readonly RefObject<HTMLElement>[], handler: () => void) {
+export function useClickOutside(
+  refs: readonly RefObject<HTMLElement>[],
+  handler: (event: MouseEvent) => void
+) {
   useEffect(() => {
     const handleClick = (event: MouseEvent) => {
       const target = event.target as Node
 
       const callHandler = refs.every(ref => ref.current && !ref.current.contains(target))
 
-      callHandler && handler()
+      callHandler && handler(event)
     }
 
     document.addEventListener('mousedown', handleClick)
