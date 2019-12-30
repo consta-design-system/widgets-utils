a399b84696e806c9073c1911deb081476b5feb1f
Alexey Fyodorov <dev.fyodorov@gmail.com>
2019-12-30 17:42:41 +0300

    GINF-675: Добавить округление в getCalculatedSize

--- src/css.ts
+++ src/css.ts
@@ -8,7 +8,9 @@ export const updateBaseSize = (size: number, element: HTMLElement | SVGElement) 
   element.style.setProperty('--base-size', `${size}`)
 }
 
-export const getCalculatedSize = (size: number) => (size / CONSTANT_SIZE) * baseSize
+export const getCalculatedSize = (size: number) => {
+  return Math.round((size / CONSTANT_SIZE) * baseSize)
+}
 
 export const calcSize = (size: number, isVar?: boolean) => {
   const text = isVar ? `(${size} / ${CONSTANT_SIZE})` : `${size / CONSTANT_SIZE}px`
