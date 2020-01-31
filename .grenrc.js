3af32e6e30766bc9e3ad4cc216a8edc30b7b4638
evless <evless56@gmail.com>
2020-01-31 16:45:24 +0300

    GINF-697: gaz-utils: Автогенерация релизов из тэгов

--- .grenrc.js
+++ .grenrc.js
@@ -10,10 +10,10 @@ module.exports = {
     "onlyMilestones": false,
     "groupBy": false,
     template: {
-        issue: ({ text, url, name }) => (
-            `- ${name.replace(/(GINF-\d*)/, '[$1](https://jira.csssr.io/browse/$1)')} [${text}](${url})`
-        ),
-        changelogTitle: '### Изменения\n\n',
-        release: '{{body}}'
+      issue: ({ text, url, name }) => (
+        `- ${name.replace(/((GINF|TSUB)-\d*)/, '[$1](https://jira.csssr.io/browse/$1)')} [${text}](${url})`
+      ),
+      changelogTitle: '### Изменения\n\n',
+      release: '{{body}}'
     }
-}
+  }
