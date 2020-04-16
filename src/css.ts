e5114b9bc90193dd45607b456eb5df0c9843b8a8
Alexey Fyodorov <dev.fyodorov@gmail.com>
2020-04-16 10:58:51 +0300

    GDC-276: Изменить название переменной для задания базового размера конструктора в @csssr/gpn-utils

--- src/css.ts
+++ src/css.ts
@@ -1,8 +1,10 @@
 // Значение, которое никогда не меняется
 const CONSTANT_SIZE = 16
 
+const PROPERTY_NAME = '--dashboard-constructor-base-size'
+
 export const updateBaseSize = (size: number, element: HTMLElement | SVGElement) => {
-  element.style.setProperty('--base-size', `${size}`)
+  element.style.setProperty(PROPERTY_NAME, `${size}`)
 }
 
 export const getCalculatedSize = (size: number, baseSize: number) => {
@@ -11,5 +13,5 @@ export const getCalculatedSize = (size: number, baseSize: number) => {
 
 export const calcSize = (size: number, isVar?: boolean) => {
   const text = isVar ? `(${size} / ${CONSTANT_SIZE})` : `${size / CONSTANT_SIZE}px`
-  return `calc(${text} * var(--base-size))`
+  return `calc(${text} * var(${PROPERTY_NAME}))`
 }
