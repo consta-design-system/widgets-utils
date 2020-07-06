31e576199bbc1cf4bfa8541d150bb54ff1fed020
Golubov Nikita <neekitkah@gmail.com>
2020-07-06 17:21:15 +0300

    GDC-562: Проблемы в виджетах, связанные с base-size | gpn-utils
    * если переменная скейлинга не указана, используем дефолтное значение

--- src/css.ts
+++ src/css.ts
@@ -1,13 +1,13 @@
 // Значение, которое никогда не меняется
-const CONSTANT_SIZE = 16
+export const DEFAULT_BASE_SIZE = 16
 
 export const BASE_SIZE_PROPERTY_NAME = '--components-base-size'
 
 export const getCalculatedSize = (size: number, baseSize: number) => {
-  return Math.round((size / CONSTANT_SIZE) * baseSize)
+  return Math.round((size / DEFAULT_BASE_SIZE) * baseSize)
 }
 
 export const calcSize = (size: number, isVar?: boolean) => {
-  const text = isVar ? `(${size} / ${CONSTANT_SIZE})` : `${size / CONSTANT_SIZE}px`
-  return `calc(${text} * var(${BASE_SIZE_PROPERTY_NAME}))`
+  const text = isVar ? `(${size} / ${DEFAULT_BASE_SIZE})` : `${size / DEFAULT_BASE_SIZE}px`
+  return `calc(${text} * var(${BASE_SIZE_PROPERTY_NAME}, ${DEFAULT_BASE_SIZE}))`
 }
